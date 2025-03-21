e4eeef1c3e17c227b0529db9a8b594355d76da6c
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-20 22:23:42 -0300

    Adds basic VadenLinkTree widget scaffold

---
+++ vaden_generator/frontend/lib/ui/link_tree/vaden_link_tree.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class VadenLinkTree extends StatefulWidget {
+  const VadenLinkTree({super.key});
+
+  @override
+  State<VadenLinkTree> createState() => _VadenLinkTreeState();
+}
+
+class _VadenLinkTreeState extends State<VadenLinkTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
