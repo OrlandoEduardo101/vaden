8733366691fd545a6893d8b894a2b65657923a18
Thiago Ganzerli <96201486+tganzerli@users.noreply.github.com>
2025-03-24 17:52:34 -0300

    Update project_validator.dart

--- vaden_generator/frontend/lib/domain/validators/project_validator.dart
+++ vaden_generator/frontend/lib/domain/validators/project_validator.dart
@@ -6,17 +6,17 @@ class ProjectValidator extends LucidValidator<Project> {
   ProjectValidator() {
     ruleFor((p) => p.name, key: 'name') //
         .notEmpty(message: 'notEmpty')
-        .minLength(3)
-        .maxLength(50)
+        .minLength(3, message: 'minLength')
+        .maxLength(50, message: 'maxLength')
         .matchesPattern(r'^[a-z][a-z0-9_]*$', code: 'invalidName');
 
     ruleFor((p) => p.description, key: 'description') //
-        .notEmpty()
-        .minLength(3)
-        .maxLength(100);
+        .notEmpty(message: 'notEmpty')
+        .minLength(3, message: 'minLength')
+        .maxLength(100, message: 'maxLength');
 
     ruleFor((p) => p.dartVersion, key: 'dartVersion') //
-        .notEmpty()
+        .notEmpty(message: 'notEmpty')
         .matchesPattern(r'^\d+\.\d+\.\d+$', code: 'invalidVersion');
   }
 }
