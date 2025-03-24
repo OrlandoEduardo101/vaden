83f3127f774470cf76b2df9aa3c7ed6287584314
Jacob Moura <jacobaraujo7@gmail.com>
2025-03-24 19:50:20 -0300

    fix warning

--- vaden/example/lib/src/auth_configuration.dart
+++ vaden/example/lib/src/auth_configuration.dart
@@ -1,4 +1,3 @@
-import 'package:example/src/credentials.dart';
 import 'package:vaden/vaden.dart';
 
 import 'envtest.dart';
@@ -7,17 +6,6 @@ import 'envtest.dart';
 class AuthConfiguration {
   @Bean()
   Future<MyEnv> myEnv(DSON factory) async {
-    final credentials = Credentials('myUsername', 'myPassword');
-
-    final json = factory.toJson(credentials);
-
-    final cred = factory.fromJson<Credentials>(
-      {
-        'user_name': 'myUsername',
-        'password': 'myPassword',
-      },
-    );
-
     return MyEnv('http://localhost:3000', 'myToken');
   }
 }
